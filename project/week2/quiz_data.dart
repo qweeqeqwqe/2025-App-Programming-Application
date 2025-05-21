71299bec107fb1f0a8b493dd256467f329659182
qweeqeqwqe <aldalddl2@g.shingu.ac.kr>
2025-05-21 15:21:59 +0900

    Add files via upload

---
+++ project/week2/quiz_data.dart
@@ -0,0 +1,12 @@
+final List<Map<String, Object>> questions = [
+  {
+    'question': '피카츄의 타입은?',
+    'answers': ['불꽃', '물', '전기', '풀'],
+    'correct': '전기',
+  },
+  {
+    'question': '꼬부기의 진화형은?',
+    'answers': ['어니부기', '파이리', '피죤', '리자드'],
+    'correct': '어니부기',
+  },
+];
